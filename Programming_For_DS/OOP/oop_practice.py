c8f4d16f7ba7db08df8226b11589e7e5e76e9762
Seunghan <seunghany2@gmail.com>
2021-01-19 10:08:39 +0900

    oop practice basic

---
+++ Programming_For_DS/OOP/oop_practice.py
@@ -0,0 +1,46 @@
+# Write two classes Point and Line that give the same output as below:
+# >>> line = Line( Point(1, 1), Point(3, 2) )  # a line comprised of two 2D points
+# >>> line.slope() 
+# 0.5
+# >>> line.length()
+# 2.23606797749979
+import math
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def slope(self):
+        '''slope function
+         rise/run
+        y2-y1/ x2-x1'''
+        y2 = self.point_2.y
+        y1 = self.point_1.y
+        x2 = self.point_2.x
+        x1 = self.point_1.x
+        line_slope = (y2-y1)/(x2-x1)
+        return line_slope
+
+    def length(self):
+        '''
+        Find length of the line
+        sqrt((y2-y1)^2 + (x2-x1)^2)
+        '''
+        y2 = self.point_2.y
+        y1 = self.point_1.y
+        x2 = self.point_2.x
+        x1 = self.point_1.x
+        line_length = math.sqrt((y2-y1)**2 + (x2-x1)**2)
+        return line_length
+
+if __name__ == "__main__":
+    point1 = Point(1,1)
+    point2 = Point(3,2)
+    line = Line(point1,point2)
+    print("slope: ", line.slope())
+    print("length: ",line.length())
