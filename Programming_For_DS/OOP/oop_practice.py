63c63c321d01c573a104aa7fc90708f1ab07f9b0
Seunghan <seunghany2@gmail.com>
2021-01-20 14:19:38 +0900

    update

--- Programming_For_DS/OOP/oop_practice.py
+++ Programming_For_DS/OOP/oop_practice.py
@@ -5,10 +5,13 @@
 # >>> line.length()
 # 2.23606797749979
 import math
+
+
 class Point():
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+
 
 class Line(Point):
     def __init__(self, point_1, point_2) -> None:
@@ -27,10 +30,10 @@ class Line(Point):
         return line_slope
 
     def length(self) -> float:
-        '''
+        """
         Find length of the line
         sqrt((y2-y1)^2 + (x2-x1)^2)
-        '''
+        """
         y2 = self.point_2.y
         y1 = self.point_1.y
         x2 = self.point_2.x
@@ -38,9 +41,10 @@ class Line(Point):
         line_length = math.sqrt((y2-y1)**2 + (x2-x1)**2) # 픽타고라스
         return line_length
 
+
 if __name__ == "__main__":
-    point1 = Point(1,1)
-    point2 = Point(3,2)
-    line = Line(point1,point2)
+    point1 = Point(1, 1)
+    point2 = Point(3, 2)
+    line = Line(point1, point2)
     print("slope: ", line.slope())
-    print("length: ",line.length())
+    print("length: ", line.length())
