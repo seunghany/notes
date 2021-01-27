9b383318756b9b7d010a9cfc5f864b707fe95013
Seunghan <seunghany2@gmail.com>
2021-01-27 21:11:27 +0900

    working on rotateLeft

--- Programming_For_DS/Tree/Height_Balanced_Tree.py
+++ Programming_For_DS/Tree/Height_Balanced_Tree.py
@@ -1,5 +1,12 @@
+class TreeNode():
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.bf = 0  # balance factor  -> left.height - right.height
 class HBT():
     """Height Balanced Tree
+    # balanced tree: One whose subtrees differ in height by at most 1 and are themselves balanced.
     def:
     A non empty binary tree T is balanced if:
     1) left subtree of T is balanced if
@@ -25,13 +32,27 @@ D     E
     def __init__(self, root = None):
         self.root = root
 
-    def height(self, root):
+    def rotateRight(self):
+        pass
+        # clockwise rotation
+        # left child becomes parent
+        # original root becomes right child node
+        # original left child's right subtree is now attatched to left of new right tree
+    def height(self, root: TreeNode) -> int:
+        '''
+        Recieves TreeNode as a parameter
+        and returns the height of the node.
+        '''
         if root is None:
             return 0
         else:
             return 1 + max(self.height(root.left), self.height(root.right))
 
-    def isBalanced(self, root):
+    def isBalanced(self, root: TreeNode) -> bool:
+        """
+        Recieves TreeNode (root) as a parameter and
+        returns true if tree is balanced and false otherwise
+        """
         if root is None:
             return True
 
@@ -42,39 +63,61 @@ D     E
         return False
 
     # step 1 insert
-    def insert(self, newNode):
-        self.root = self.insertHelp(self.root, newNode)
+    def insert(self, newVal: int) -> None:
+        """
+        Recieves new integer value and adds a new tree node to the root
+        """
+        self.root = self.insertHelp(self.root, newVal)
         if self.isBalanced(self.root):
             print("Balanced")
         else:
             print("Not Balanced")
-            # 만약 unBalnced 하다면 rotation이 필요
+            # 만약 unbalanced 하다면 rotation 필요
+            # Right Right	==> Z is a right	child of its parent X and BF(Z) ≥ 0
+            # Left Left	==> Z is a left	child of its parent X and BF(Z) ≤ 0
+            # Right Left	==> Z is a right	child of its parent X and BF(Z) < 0
+            # Left Right	==> Z is a left	child of its parent X and BF(Z) > 0
 
-    def insertHelp(self, curNode, newNode):
+            # And the rebalancing is performed differently:
+            #
+            # Right Right	==> X is rebalanced with a	simple	rotation rotate_Left
+            # Left Left	==> X is rebalanced with a	simple	rotation rotate_Right
+            # Right Left	==> X is rebalanced with a	double	rotation rotate_RightLeft
+            # Left Right	==> X is rebalanced with a	double	rotation rotate_LeftRight
+    def insertHelp(self, curNode, newVal):
         # Base Case
         if not curNode:  # 만약 끝에 도착한다면 새로운 노드 안착
-            return TreeNode(newNode)
-        if newNode == curNode.val:
+            return TreeNode(newVal)
+        if newVal == curNode.val:
             return curNode
-        # recursie case
-        if curNode.val < newNode:  # 숫자가 작으면 왼쪽으로
-            curNode.left = self.insertHelp(curNode.left, newNode)
+        # recursive case
+        if curNode.val < newVal:  # 숫자가 작으면 왼쪽으로
+            curNode.left = self.insertHelp(curNode.left, newVal)
         else: # 숫자가 크면 오른쪽으로
-            curNode.right = self.insertHelp(curNode.right, newNode)
+            curNode.right = self.insertHelp(curNode.right, newVal)
 
         return curNode
 
+    def rotation(self, curNode):
+        # left rotation
+        newRoot = curNode
+        newRoot = newRoot.right
+        oldLeft = newRoot.left  # 여기서 에러남
+        newRoot.left = curNode
+        curNode.right = oldLeft
+        self.isBalanced(newRoot)
+        return newRoot
 
 
 
-class TreeNode():
-    def __init__(self, val, left = None, right = None):
-        self.val = val
-        self.left = left
-        self.right = right
 
 if __name__ == '__main__':
     Tree = HBT()
     Tree.insert(5)
     Tree.insert(6)
     Tree.insert(7)
+    Tree.rotation(Tree.root)
+    # right right
+    # need left rotation
+
+    # unbalanced
