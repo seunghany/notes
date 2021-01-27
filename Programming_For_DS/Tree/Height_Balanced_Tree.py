cc51317efe736161da1b926090615a1115d5fe4a
Seunghan <seunghany2@gmail.com>
2021-01-27 18:49:11 +0900

    checking if tree balanced done now I need to work on rotation

--- Programming_For_DS/Tree/Height_Balanced_Tree.py
+++ Programming_For_DS/Tree/Height_Balanced_Tree.py
@@ -25,24 +25,44 @@ D     E
     def __init__(self, root = None):
         self.root = root
 
+    def height(self, root):
+        if root is None:
+            return 0
+        else:
+            return 1 + max(self.height(root.left), self.height(root.right))
+
+    def isBalanced(self, root):
+        if root is None:
+            return True
+
+        lh = self.height(root.left)  # left height
+        rh = self.height(root.right)  # right height
+        if abs(lh-rh) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+            return True
+        return False
+
     # step 1 insert
     def insert(self, newNode):
-        if type(newNode) == int:
-            newNode = TreeNode(newNode)
         self.root = self.insertHelp(self.root, newNode)
+        if self.isBalanced(self.root):
+            print("Balanced")
+        else:
+            print("Not Balanced")
+            # 만약 unBalnced 하다면 rotation이 필요
 
     def insertHelp(self, curNode, newNode):
-        # if Tree is empty, the new node becomes the root
-        if self.root is None:
-            self.root = newNode
-
+        # Base Case
         if not curNode:  # 만약 끝에 도착한다면 새로운 노드 안착
-            return newNode
-
-        if curNode.val < newNode.val:  # 숫자가 작으면 왼쪽으로
+            return TreeNode(newNode)
+        if newNode == curNode.val:
+            return curNode
+        # recursie case
+        if curNode.val < newNode:  # 숫자가 작으면 왼쪽으로
             curNode.left = self.insertHelp(curNode.left, newNode)
         else: # 숫자가 크면 오른쪽으로
             curNode.right = self.insertHelp(curNode.right, newNode)
+
+        return curNode
 
 
 
@@ -57,3 +77,4 @@ if __name__ == '__main__':
     Tree = HBT()
     Tree.insert(5)
     Tree.insert(6)
+    Tree.insert(7)
